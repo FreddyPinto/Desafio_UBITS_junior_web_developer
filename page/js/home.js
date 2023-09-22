5c539f913071a65ac5d7463a60a44bb62816306b
FreddyPinto <freddypinto@outlook.com>
2023-09-21 21:05:20 -0500

    home solved

--- page/js/home.js
+++ page/js/home.js
@@ -1,53 +1,70 @@
-// como puedes observar tenemos la informacion de nuestro plan incompleta vamos a solucioanr este inconveniente
-
 const init = async () => {
-  // 1. llama la API con la siguiente URL https://buty619.github.io/pricing.json donde traera
-  // toda la información de url para luego inyectarla en nuestro HTML, para esto primero
-  // realiza una petición GET para obtener dicha data.
-  // NOTA: puedes abrir la url en tu navegador para ver la estructura de la información obtenida.
+  // 1. Se llama a la API con la siguiente URL https://buty619.github.io/pricing.json que traera
+  // toda la información para luego inyectarla en el HTML realizando una petición GET para obtener dicha data.
+    const url = "https://buty619.github.io/pricing.json";
+  const jsonData = await fetch(url);
+  const { basic, eco, pro, business } = await jsonData.json();
 
-  const url = "https://buty619.github.io/pricing.json";
-  // 2. luego de obtenida la información de los planes se debe inyectar los valores obtenidos
+  // 2. Luego de obtener la información de los planes se inyectan los valores
   // en el html de cada card de los posibles planes a comprar.
-  // 2.1 primero se debe obtener el elemento card del plan basic para esto puedes usar su clase (validalo en el HTML)
-  const basicCard = ...;
-  // 2.1.1 del elemento obtenido selecciona el titulo e inyecta el valor obtenido de la API
-  const basicCardTitle = ...;
-  // 2.1.2 del elemento card obtenido selecciona el precio e inyecta el valor obtenido de la API ten en cuenta que
-  // el componente del precio esta compuesto por los tags de small y span valida esto al momento de inyectar
-  // el valor obtenido de la llamada a la API
-  const basicCardPrice = ...;
-  const basicCardPriceSpan = ...;
-  // 2.1.3  ahora inyecta el valor del descuento de este plan en el span que se encuentra dentro del elemento
+
+  // 2.1 Primero se obtiene el elemento card del plan basic usando su clase.
+  const basicCard = document.querySelector(".pricing-card.basic");
+
+  // 2.1.1 Se selecciona el titulo y se inyecta el valor obtenido de la API.
+  const basicCardTitle = basicCard.querySelector(".plan-title");
+  basicCardTitle.innerHTML = basic.name;
+
+  // 2.1.2 Se selecciona el precio y se inyecta el valor obtenido de la API en la tag span correspondiente.
+    const basicCardPrice = basicCard.querySelector(".price-title");
+  const basicCardPriceSpan = basicCardPrice.querySelector("span");
+  basicCardPriceSpan.innerHTML = basic.price;
+
+  // 2.1.3  Ahora se inyecta el valor del descuento de plan basic en el span que se encuentra dentro del elemento
   // con clase `badge-box`.
-  const basicCardDiscount = ...;
-  const basicCardDiscountSpan = ...;
-  // 2.1.4 por ultimo completa la informacion de las caracteristicas del plan para esto tambien ten en cuenta
-  // la estructura del HTML asi como la estructura de la información donde se encuentra las caracteristicas del plan.
-  // NOTA: para este paso te recomendamos obtener un array con los elementos li y recorrerlos agregando las caracteristicas
-  // del plan
-  const basicCardList = ...;
-  const basicCardElementList = ...;
+  const basicCardDiscount = basicCard.querySelector(".badge-box");
+  const basicCardDiscountSpan = basicCardDiscount.querySelector("span");
+  basicCardDiscountSpan.innerHTML = `Save ${basic.discount}`;
+
+  // 2.1.4 Por ultimo se completa la informacion de las caracteristicas del plan
+  //utilizando un array con los elementos li y recorriendolo con la función map para 
+  //cambiar el contenido de cada elemento li por la correspondiente caracteristica del plan.
+  const basicCardList = basicCard.querySelector("ul");
+  const basicCardElementList = basicCardList.querySelectorAll("li");
   [...basicCardElementList].map(
-    (element, i) => {...}
+    (element, i) => (element.innerHTML = basic.characteristics[i])
   );
-  // 2.2 sigue los pasos anteriormente mencionados para llenar la información de cada uno de los planes,
-  // esto lo puedes hacer realizando el proceso para cada card o realizando una iteración que te permita
-  // re utilizar el codigo inplementado para la primera card, cualquiera de las dos soluciones es valida
-  // sin embargo que intivo a que pruebes tus conocimientos y trates de solucionar esta parte del reto
-  // utilizando un metodo de iteración sobre la data obtenida en la API.
+
+  // 2.2  Ahora utilizando nuevamente la funcion map se re utiliza el codigo inplementado
+  // en la primera card para llenar la información del resto de cards
+  // con cada uno de los planes correspondientes.
+  const apiData = { basic, eco, pro, business };
   Object.entries(apiData).map(([section, data]) => {
-    ...
+    const card = document.querySelector(`.pricing-card.${section}`);
+    const cardTitle = card.querySelector(".plan-title");
+    cardTitle.innerHTML = data.name;
+    const cardPrice = card.querySelector(".price-title");
+    const cardPriceSpan = cardPrice.querySelector("span");
+    cardPriceSpan.innerHTML = data.price;
+    const cardDiscount = card.querySelector(".badge-box");
+    const cardDiscountSpan = cardDiscount.querySelector("span");
+    cardDiscountSpan.innerHTML = `Save ${data.discount}`;
+    const cardList = card.querySelector("ul");
+    const cardElementList = cardList.querySelectorAll("li");
+    [...cardElementList].map(
+      (element, i) => (element.innerHTML = data.characteristics[i])
+    );
   });
-  // 3. Agrega una accion a los botones de cada card que nos dirija la pagina `/payment` donde
-  // envies como query params el nombre del plan y el precio. ejemplo: /payment.html?name='basic'&price='$6.95'
-  // al igual que el paso anterior esto lo puedes hacer de manera indivudual o recorriendo a una iteración
-  // sobre la información obtenida
-  Object.entries(apiData).map(([section, data]) => {
-    ...
+
+  // 3. Por ultimo, se agrega una accion a los botones de cada card que nos dirije la pagina `/payment` que
+  // envia como query params el nombre del plan y el precio.
+Object.entries(apiData).map(([section, data]) => {
+    const card = document.querySelector(`.pricing-card.${section}`);
+    const cardButton = card.querySelector(".buy-now");
+    cardButton.href = `/payment.html?name=${data.name}&price=$${data.price}`;
   });
 };
 
-// se inicializa el script
+// Se inicializa el script
 
 init();
